f8b05f2824ebf22d3326c8d6e54b68f28a727e59
vishnuu <vishnuu@terpmail.umd.edu>
2020-06-25 18:34:41 -0400

    plane fit done linear regression solution

--- src/visualise_depth.py
+++ src/visualise_depth.py
@@ -6,11 +6,11 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge
 import sys
-global reset, refPt, ctrl, drawing, completed, img_held, img, prev_ctrl
+global reset, refPt, ctrl, drawing, completed, img_held, img, prev_ctrl, img_depth, img_depth_held
 
 
-def handler(data):
-	global reset, refPt, ctrl, drawing, completed, img_held, img
+def handler_img(data):
+	global reset, refPt, ctrl, drawing, completed, img_held, img, img_depth, img_depth_held
 	# rospy.loginfo("Image Message received!" )
 	# rospy.loginfo("Header: " +  str(data.header))
 	# rospy.loginfo("Height: " +  str(data.height))
@@ -21,11 +21,15 @@ def handler(data):
 
 	# filewriter.write(bridge.imgmsg_to_cv2(data, "bgr8"))
 	img = bridge.imgmsg_to_cv2(data, "bgr8")
+	# print(type(img))
 	# param = [img]
 
+def handler_depth(data):
+	global reset, refPt, ctrl, drawing, completed, img_held, img, img_depth
+	img_depth = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
 	
 def mouse_callback(event, x, y, flags, param):
-	global reset, refPt, ctrl, drawing, completed, img_held, prev_ctrl, img
+	global reset, refPt, ctrl, drawing, completed, img_held, prev_ctrl, img, img_depth_held
 
 	if (completed != True):
 		prev_ctrl = ctrl
@@ -38,6 +42,7 @@ def mouse_callback(event, x, y, flags, param):
 
 		if(prev_ctrl==False and ctrl==True):
 			img_held = img.copy()
+			img_depth_held = img_depth.copy()
 			refPt = []
 
 		
@@ -64,13 +69,19 @@ if __name__=="__main__":
 	# filewriter = cv2.VideoWriter("~/test_video", cv2.VideoWriter_fourcc(*'MJPG'), 24, (640,480))
 	bridge = CvBridge()
 	img_held = np.array([], dtype=np.uint8)
-	img = np.array([], dtype = np.uint8)
+	img = np.zeros([], dtype = np.uint8)
 	cv2.namedWindow("img_window")
 
 	rospy.init_node("visualise_depth", anonymous=True)
 
-	rospy.Subscriber("camera/color/image_raw", Image, handler) # Color image subscribe
+	
 	rospy.wait_for_message("camera/color/image_raw", Image)
+	rospy.Subscriber("camera/color/image_raw", Image, handler_img)
+
+	
+	rospy.wait_for_message("camera/aligned_depth_to_color/image_raw", Image)
+	rospy.Subscriber("camera/aligned_depth_to_color/image_raw", Image, handler_depth)
+
 	cv2.setMouseCallback("img_window", mouse_callback)
 	# rospy.Subscriber("camera/depth/image_rect_raw", Image, handler) # Depth image subscribe
 	
@@ -80,7 +91,8 @@ if __name__=="__main__":
 	prev_ctrl = False
 	reset = False
 	loop = True
-
+	
+	# rospy.wait_for_message("camera/color/image_raw", Image)
 	while(loop):
 		key = cv2.waitKey(1) & 0xFF
 		if (key == ord("r") or key == ord("R")):
@@ -116,6 +128,8 @@ if __name__=="__main__":
 		# print refPt
 		print "--------"
 		img_held2 = img_held.copy()
+
+		#**************
 		# hull = cv2.convexHull(refPt)
 		# hull_list = [hull]
 		# drawing = np.zeros((img_held.shape[0], img_held.shape[1], 3), dtype = np.uint8)
@@ -124,6 +138,8 @@ if __name__=="__main__":
 
 		# cv2.imshow("contours",drawing)
 		# cv2.waitKey(0)
+		#**************
+
 		############ METHOD TO FIND ALL POINTS INSIDE POLYGON ###################
 		for i in range(len(refPt)):
 			# x = ((x2-x1)/(y2-y1))*y + c
@@ -168,6 +184,8 @@ if __name__=="__main__":
 		img_held2 = img_held.copy()
 		# print(refPt_fill)
 		refPt_fill = np.array(refPt_fill)
+
+		#**************
 		# print(refPt_fill.shape)
 		# print(refPt_fill[0])
 		# print(refPt_fill[1])
@@ -178,6 +196,8 @@ if __name__=="__main__":
 		# cv2.imshow("next_window", img_held2)
 		# cv2.waitKey(0)
 		# print refPt_fill
+		#**************
+
 		cv2.destroyAllWindows()
 		img_mask = np.full((img_held.shape), False, dtype=np.bool)
 		sortedPts = np.sort(refPt_fill.view('i8,i8'), order=['f0'], axis=0).view(np.int)
@@ -190,6 +210,8 @@ if __name__=="__main__":
 			anc = sortedPts[i]
 		
 		sortedPts = sortedPts[sort_mask].reshape(-1,2)
+
+		#**************
 		# for j in range(	len(sortedPts)):
 		# 	img_held2 = cv2.circle(img_held2, tuple(sortedPts[j]), 1, (0,0,255),0)
 		# # # # print ("end of itrs", refPt_fill)
@@ -198,6 +220,9 @@ if __name__=="__main__":
 		# print sortedPts.shape
 		# print sortedPts
 		# input()
+		#**************
+
+
 		anc = sortedPts[0]
 		# val = False
 		lst = np.array([sortedPts[0]])
@@ -211,7 +236,7 @@ if __name__=="__main__":
 				for j in range(lst.shape[0]-1):
 					# print ("j: ",j)
 					if (lst[j+1][1] - lst[j][1]) > 1:
-						print(lst[0][0])
+						# print(lst[0][0])
 						y = np.arange(lst[j][1], lst[j+1][1])
 						x = np.full_like(y, lst[0][0])
 						# print(x,y)
@@ -223,13 +248,36 @@ if __name__=="__main__":
 				lst = np.array([sortedPts[i]])	
 			anc = sortedPts[i]
 
+		#visualise regular image
 		img_held2[img_mask] = 0
 		cv2.imshow("next_window", img_held2)
 		cv2.waitKey(0)
-		
-		
+
+
+		# #visualise depth image cutout
+		img_depth_held2 = img_depth_held.copy()
+		img_depth_held2[img_mask[:,:,0]] = 0
+		cv2.imshow("next_window", img_depth_held2)
+		cv2.waitKey(0)
+		# print("depth vals: ",img_depth_held[img_mask[:,:,0]])
+
+		idx = np.where(img_mask[:,:,0] == True)
+		idx = np.array(idx).T
+		# # print (idx.shape)
+		pcl = np.hstack((idx, img_depth_held[img_mask[:,:,0]][:,None]))
+		# print pcl.shape
+
+		# ax + by + c = z equation of line
+		ones = np.ones((pcl.shape[0],1))
+		A = np.hstack((idx, ones))
+		# print A
+		z = img_depth_held[img_mask[:,:,0]][:,None]
+		# print z
+		xTx_inv = np.linalg.inv(np.matmul(A.T, A))
+		xTy = np.matmul(A.T, z)
+		sol = np.matmul(xTx_inv, xTy)
+		print sol
 
 		
-	
 
 	cv2.destroyAllWindows()
